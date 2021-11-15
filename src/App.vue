cfebc92394bf56ea3d94073212fb6a8aae289007
lumilio <logistic-division@lumilium.com>
2021-11-15 15:09:13 +0100

    brainstorming...

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ export default {
         HeaderBanner, 
         CardsContainer,
     },
+    props: {},
 }
 </script>
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card-model">
         <img :src="poster" alt="">
-        <h2>{{title.toUpperCase()}}</h2>
-        <span>{{title}}</span>
+        <h2>{{title}}</h2>
+        <span>{{author}}</span>
         <span>{{year}}</span>
     </div>
 </template>
@@ -34,18 +34,4 @@ export default {
 /* -------------------------------------------------------------------------- */
 
 <style scoped lang="scss">
-.card-model {
-    padding: 0.6rem;
-}
-.card-model img{
-    width: 170px;
-    height: 170px;
-    object-fit: cover;
-    object-position: center top;
-}
-.card-model p{
-    width: 120px;
-    margin-top: 8px;
-    margin-bottom: 16px;
-}
 </style>
